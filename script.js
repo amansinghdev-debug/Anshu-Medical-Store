3a1bc1e8923eea798f48766d8345358329ab21bf
Aman Singh <amankumar748090@gmail.com>
2025-06-06 21:06:06 +0530

    Update and rename a.js to script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const navLinks = document.getElementById('navLinks');
+
+function navOpen(){
+    navLinks.classList.add('active');
+}
+function CloseNav(){
+    navLinks.classList.remove('active');
+}
